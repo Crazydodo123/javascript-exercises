759f0cd43a9cc8c317275dd2998bb81b42b27854
Crazydodo123 <peizheg@gmail.com>
2023-06-01 17:18:50 -0400

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removed) {
+    ans = []
 
+    for (ele of array) {
+
+        if (!removed.includes(ele)) ans.push(ele)
+    }
+
+    return ans
 };
 
 // Do not edit below this line

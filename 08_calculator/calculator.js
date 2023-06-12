8ef9f5cf73648663338a86fa14da14af6e40669c
Crazydodo123 <peizheg@gmail.com>
2023-06-12 19:36:56 -0400

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, ele) => acc += ele, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, ele) => acc *= ele, 1)
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  ans = 1
+	for (let i = 1; i <= power; i++) {
+    ans *= base
+  }
+
+  return ans
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	ans = 1
+
+  while (num > 1) {
+    ans *= num
+    num--
+  }
+
+  return ans
 };
 
 // Do not edit below this line

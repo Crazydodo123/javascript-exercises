6caf3b56382a74d21c693b9aca1659b9896dea50
Crazydodo123 <peizheg@gmail.com>
2023-06-12 19:55:54 -0400

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const tested = "abcdefghijklmnopqrstuvwxyz1234567890"
+    chars = []
+    sentence = sentence.toLowerCase()
 
+    for (const char of sentence) {
+        if (tested.includes(char)) chars.push(char)
+    }
+
+    for (let i = 0; i < (chars.length / 2); i++) {
+
+        if (chars[i] !== chars[chars.length - i - 1]) return false
+    }
+
+    return true
 };
 
 // Do not edit below this line

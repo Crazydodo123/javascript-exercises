28c4a148c5a544cd35444f0c344df916e6e21881
Crazydodo123 <peizheg@gmail.com>
2023-06-14 17:51:41 -0400

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((personA, personB) => {
+        const ageA = (personA.yearOfDeath || 2023) - personA.yearOfBirth;
+        const ageB = (personB.yearOfDeath || 2023) - personB.yearOfBirth;
 
+        return (ageB - ageA)
+    });
+    return people[0]
 };
 
 // Do not edit below this line
